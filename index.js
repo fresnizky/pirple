03bd3f3ccf6834779eefcb34da60b9a2d57aeb51
fresnizky <fresnizky@yahoo.com.ar>
2018-10-25 23:59:43 -0300

    Check if certificate files exists before starting https server (#1)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
  * Primary file for API
- *
+ * 
  */
 
 // Dependencies
@@ -11,34 +11,36 @@ var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
 var fs = require('fs');
 
- // Instantiating the HTTP server
-var httpServer = http.createServer(function(req,res){
-    unifiedServer(req, res);
+// Instantiating the HTTP server
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
 });
 
 // Start the server
-httpServer.listen(config.httpPort,function(){
+httpServer.listen(config.httpPort, function () {
   console.log('The server is listening on port ' + config.httpPort);
 });
 
-// Instantiate the HTTPS server
-var httpsServerOptions = {
+// Check if certificate files are present
+if (fs.existsSync('./https/key.pem') && fs.existsSync('./https/cert.pem')) {
+  // Instantiate the HTTPS server
+  var httpsServerOptions = {
     'key': fs.readFileSync('./https/key.pem'),
-    'cert':  fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions, function(req,res){
+    'cert': fs.readFileSync('./https/cert.pem')
+  };
+  var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
     unifiedServer(req, res);
-});
+  });
 
 
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort,function(){
+  // Start the HTTPS server
+  httpsServer.listen(config.httpsPort, function () {
     console.log('The server is listening on port ' + config.httpsPort);
   });
-  
+}
 
 // All the server logic for both the http and https server
-var unifiedServer = function(req, res) {
+var unifiedServer = function (req, res) {
   // Parse the url
   var parsedUrl = url.parse(req.url, true);
 
@@ -58,61 +60,61 @@ var unifiedServer = function(req, res) {
   // Get the payload,if any
   var decoder = new StringDecoder('utf-8');
   var buffer = '';
-  req.on('data', function(data) {
-      buffer += decoder.write(data);
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
   });
-  req.on('end', function() {
-      buffer += decoder.end();
+  req.on('end', function () {
+    buffer += decoder.end();
 
-      // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
-      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+    var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-      // Construct the data object to send to the handler
-      var data = {
-        'trimmedPath' : trimmedPath,
-        'queryStringObject' : queryStringObject,
-        'method' : method,
-        'headers' : headers,
-        'payload' : buffer
-      };
+    // Construct the data object to send to the handler
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    };
 
-      // Route the request to the handler specified in the router
-      chosenHandler(data,function(statusCode,payload){
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function (statusCode, payload) {
 
-        // Use the status code returned from the handler, or set the default status code to 200
-        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // Use the status code returned from the handler, or set the default status code to 200
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
 
-        // Use the payload returned from the handler, or set the default payload to an empty object
-        payload = typeof(payload) == 'object'? payload : {};
+      // Use the payload returned from the handler, or set the default payload to an empty object
+      payload = typeof (payload) == 'object' ? payload : {};
 
-        // Convert the payload to a string
-        var payloadString = JSON.stringify(payload);
+      // Convert the payload to a string
+      var payloadString = JSON.stringify(payload);
 
-        // Return the response
-        res.setHeader('Content-Type', 'application/json');
-        res.writeHead(statusCode);
-        res.end(payloadString);
-        console.log("Returning this response: ",statusCode,payloadString);
+      // Return the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log("Returning this response: ", statusCode, payloadString);
 
-      });
+    });
 
-  });   
+  });
 };
 
 // Define all the handlers
 var handlers = {};
 
 // Ping handler
-handlers.ping = function(data, callback) {
-    callback(200);
+handlers.ping = function (data, callback) {
+  callback(200);
 }
 
 // Not found handler
-handlers.notFound = function(data,callback){
+handlers.notFound = function (data, callback) {
   callback(404);
 };
 
 // Define the request router
 var router = {
-  'ping' : handlers.ping
+  'ping': handlers.ping
 };
