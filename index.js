05a0f0a094b259e62d2e73735878a623fb4fdd5d
fresnizky <fresnizky@yahoo.com.ar>
2018-10-28 11:35:37 -0300

    Feature/refator es6 (#4)
    * Refactored to ES6
    
    * Renamed to index.js

--- index.js
+++ index.js
@@ -4,76 +4,76 @@
  */
 
 // Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
-var fs = require('fs');
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
 
 // Instantiating the HTTP server
-var httpServer = http.createServer(function (req, res) {
+const httpServer = http.createServer((req, res) => {
   unifiedServer(req, res);
 });
 
 // Start the server
-httpServer.listen(config.httpPort, function () {
-  console.log('The server is listening on port ' + config.httpPort);
+httpServer.listen(config.httpPort, () => {
+  console.log(`The server is listening on port ${config.httpPort}`);
 });
 
 // Check if certificate files are present
 if (fs.existsSync('./https/key.pem') && fs.existsSync('./https/cert.pem')) {
   // Instantiate the HTTPS server
-  var httpsServerOptions = {
+  const httpsServerOptions = {
     'key': fs.readFileSync('./https/key.pem'),
     'cert': fs.readFileSync('./https/cert.pem')
   };
-  var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
     unifiedServer(req, res);
   });
 
 
   // Start the HTTPS server
-  httpsServer.listen(config.httpsPort, function () {
-    console.log('The server is listening on port ' + config.httpsPort);
+  httpsServer.listen(config.httpsPort, () => {
+    console.log(`The server is listening on port ${config.httpsPort}`);
   });
 }
 
 // All the server logic for both the http and https server
-var unifiedServer = function (req, res) {
+var unifiedServer = (req, res) => {
   // Parse the url
-  var parsedUrl = url.parse(req.url, true);
+  const parsedUrl = url.parse(req.url, true);
 
   // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
   // Get the query string as an object
-  var queryStringObject = parsedUrl.query;
+  const queryStringObject = parsedUrl.query;
 
   // Get the HTTP method
-  var method = req.method.toLowerCase();
+  const method = req.method.toLowerCase();
 
   //Get the headers as an object
-  var headers = req.headers;
+  const headers = req.headers;
 
   // Verify if the request is encrypted
   const isSSL = typeof(req.socket.encrypted) == 'boolean' ? req.socket.encrypted : false;
 
   // Get the payload,if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', function (data) {
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', data => {
     buffer += decoder.write(data);
   });
-  req.on('end', function () {
+  req.on('end', () => {
     buffer += decoder.end();
 
     // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
-    var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
     // Construct the data object to send to the handler
-    var data = {
+    const data = {
       'trimmedPath': trimmedPath,
       'queryStringObject': queryStringObject,
       'method': method,
@@ -83,7 +83,7 @@ var unifiedServer = function (req, res) {
     };
 
     // Route the request to the handler specified in the router
-    chosenHandler(data, function (statusCode, payload) {
+    chosenHandler(data, (statusCode, payload) => {
 
       // Use the status code returned from the handler, or set the default status code to 200
       statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
@@ -92,7 +92,7 @@ var unifiedServer = function (req, res) {
       payload = typeof (payload) == 'object' ? payload : {};
 
       // Convert the payload to a string
-      var payloadString = JSON.stringify(payload);
+      const payloadString = JSON.stringify(payload);
 
       // Return the response
       res.setHeader('Content-Type', 'application/json');
@@ -109,18 +109,18 @@ var unifiedServer = function (req, res) {
 var handlers = {};
 
 // Ping handler
-handlers.ping = function (data, callback) {
+handlers.ping = (data, callback) => {
   callback(200);
 }
 
 // Hello handler
-handlers.hello = function (data, callback) {
+handlers.hello = (data, callback) => {
   const msg = (data.isSSL) ? 'Hello World!!' : 'You have been hacked !!!';
   callback(200, {'msg': msg});
 }
 
 // Not found handler
-handlers.notFound = function (data, callback) {
+handlers.notFound = (data, callback) => {
   callback(404);
 };
 
