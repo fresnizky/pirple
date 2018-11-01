b1654bea963c78998e91cf26dfabead36a7e2300
Federico Resnizky <federico.resnizky@intive-fdv.com>
2018-11-01 19:51:27 -0300

    Added checks handler Removed checks when deleting user

--- index.js
+++ index.js
@@ -112,5 +112,6 @@ const router = {
   'ping': handlers.ping,
   'hello': handlers.hello,
   'users': handlers.users,
-  'tokens': handlers.tokens
+  'tokens': handlers.tokens,
+  'checks': handlers.checks
 };
